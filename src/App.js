e1402b29ea9b21e12250c156a964c11c897f8044
Mohit <mohit.saran@gcp.lowes.com>
2021-05-02 19:13:26 +0530

    Covid Appointment Slot

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+import checkAvailability from "./service";
+import "./style.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pin: 0,
+      result: [],
+    };
+  }
+
+  onChange = (event) => {
+    this.setState({ pin: event.target.value });
+  };
+
+  handleSubmit = async (event) => {
+    event.preventDefault();
+    const results = await checkAvailability(parseInt(this.state.pin));
+    this.setState({ result: results });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <form onSubmit={this.handleSubmit}>
+          Enter Pincode:{" "}
+          <input value={this.state.term} onChange={this.onChange} />
+          <button>Search!</button>
+        </form>
+        <br></br>
+        <table id="tab">
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Name</th>
+              <th>Time</th>
+              <th>Available Capacity</th>
+              <th>Vaccine</th>
+              <th>Slots</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.result.map((element, index) => (
+              <tr key={index}>
+                <td>{element.date}</td>
+                <td>{element.name}</td>
+                <td>
+                  {element.from} - {element.to}
+                </td>
+                <td>{element.available_capacity}</td>
+                <td>{element.vaccine}</td>
+                <td>{element.slots}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/service.js
@@ -0,0 +1,53 @@
+const moment = require("moment");
+const axios = require("axios");
+
+async function getSlotsForDate(PINCODE, DATE) {
+  let config = {
+    method: "get",
+    url:
+      "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=" +
+      PINCODE +
+      "&date=" +
+      DATE,
+    headers: {
+      accept: "application/json",
+    },
+  };
+
+  const response = await axios(config);
+  return response;
+}
+
+async function fetchNext10Days() {
+  let dates = [];
+  let today = moment();
+  for (let i = 0; i < 30; i++) {
+    let dateString = today.format("DD-MM-YYYY");
+    dates.push(dateString);
+    today.add(1, "day");
+  }
+  return dates;
+}
+
+async function checkAvailability(pin) {
+  var slotListArray = [];
+  let datesArray = await fetchNext10Days();
+
+  const getSlots = async () => {
+    for (let i = 0; i <= datesArray.length; i++) {
+      const output = await getSlotsForDate(pin, datesArray[i]);
+      let sessions = output.data.sessions;
+      let validSlots = sessions.filter(
+        (slot) => slot.min_age_limit > "18" && slot.available_capacity > 0
+      );
+      if (validSlots.length) {
+        slotListArray.push(validSlots[0]);
+      }
+    }
+    return slotListArray;
+  };
+  const respslot = await getSlots();
+  return respslot;
+}
+
+export default checkAvailability;
